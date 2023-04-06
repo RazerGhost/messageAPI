b76c420c002869e6919961d514f4cdf6ad8006cd
RazerGhost <IRazerIGhost@gmail.com>
2023-04-06 18:16:55 +0200

    Message API

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('comments')->paginate(20);
+        return view('index', compact('posts'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        return view('show', compact('post'));
+    }
+}
